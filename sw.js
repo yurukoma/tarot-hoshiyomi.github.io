bba46daeb188b0d0453e91cd696468677226e374
yurukoma <da00489@gmail.com>
2025-12-11 10:44:36 +0900

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,39 @@
+// 簡易キャッシュ
+const CACHE_NAME = 'yurutto-tarot-cache-v1';
+const URLS_TO_CACHE = [
+  './',
+  './index.html'
+];
+
+// インストール
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(URLS_TO_CACHE);
+    })
+  );
+});
+
+// 有効化
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((name) => {
+          if (name !== CACHE_NAME) {
+            return caches.delete(name);
+          }
+        })
+      );
+    })
+  );
+});
+
+// fetch 時キャッシュ or ネットワーク
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
